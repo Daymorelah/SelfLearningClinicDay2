2e830532a7e0a64791056419146b0f666b215f1b
Daymorelah <demola.hussainin@gmail.com>
2017-07-04 12:13:19 +0100

    Initialize the directory as a Node project, created and added file names to .gitignore file.

---
+++ src/gradeBook.js
@@ -0,0 +1,3 @@
+/** Create an OOP implementation by modelling a real-world scenario/problem, 
+while taking advantage of inheritance, encapsulation, polymorphism and the 
+other OOP concepts. Push this solution to GitHub. **/
